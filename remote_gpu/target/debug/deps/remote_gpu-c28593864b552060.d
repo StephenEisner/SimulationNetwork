4ffd1fa236fc049dcea4281eda1245a9add22b68
StephenEisner <stephen.s.eisner@gmail.com>
2021-12-30 22:46:40 -0500

    remote gpu based prototype repo

---
+++ remote_gpu/target/debug/deps/remote_gpu-c28593864b552060.d
@@ -0,0 +1,5 @@
+/Users/stepheneisner/Desktop/EmlynProject/SimulationNetwork/remote_gpu/target/debug/deps/remote_gpu-c28593864b552060: src/main.rs
+
+/Users/stepheneisner/Desktop/EmlynProject/SimulationNetwork/remote_gpu/target/debug/deps/remote_gpu-c28593864b552060.d: src/main.rs
+
+src/main.rs:
---
+++ remote_gpu/target/debug/remote_gpu.d
@@ -0,0 +1,1 @@
+/Users/stepheneisner/Desktop/EmlynProject/SimulationNetwork/remote_gpu/target/debug/remote_gpu: /Users/stepheneisner/Desktop/EmlynProject/SimulationNetwork/remote_gpu/src/main.rs
